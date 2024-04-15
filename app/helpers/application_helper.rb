3f9ee85c5bf1c79d95012eb2064e90437eb298c3
Fabiana M. M. Rodrigues <biamunhoz@gmail.com>
2024-04-15 15:51:08 -0300

    Primeiro Commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,22 @@
+module ApplicationHelper
+
+    def carregarsalas
+
+        @salas = carrega_salas
+
+        salaspermitidas = Array.new
+
+        @salas.each do |a|
+            salaspermitidas << a.id
+        end
+
+        return salaspermitidas
+    end
+
+    def teminscricao(agendaid)
+        @inscricao = Inscricao.joins(:usuario).joins(:agenda).where("usuarios.loginUsuario = ? and agenda_id = ? ", session[:login], agendaid)
+        .select("usertipo, agenda_id, inscricaos.tipo")
+    end 
+  
+   
+end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'naoresponder@chagas.icb.usp.br'
+  layout 'mailer'
+end
